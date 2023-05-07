fcfe0ae38ded1aa92d900ff44b628d9891d1cf5d
AbinashSelvaraj <abinash@lcscontrols.in>
2023-05-07 18:39:56 +0530

    update header

--- lcs-tippermonitoringsystem/docusaurus.config.js
+++ lcs-tippermonitoringsystem/docusaurus.config.js
@@ -74,9 +74,9 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Applications',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog', label: 'Manuals', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'About',
